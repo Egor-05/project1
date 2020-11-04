852a60ac2abde67b058e4c680149197608774da9
egor <e.kulpin@mail.ru>
2020-11-04 20:32:37 +0300

    MVP

--- main.py
+++ main.py
@@ -1,34 +1,89 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
-from PyQt5.QtGui import QPainter, QPixmap
+from PyQt5.QtGui import QPainter, QPixmap, QColor
 from window import Ui_Dialog
-import csv
+import sqlite3
+from random import randint
 
 
 class MyWidget(QMainWindow, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.btn_yes.clicked.connect()
-        # self.btn_no.clicked.connect()
+        self.btn_yes.clicked.connect(self.yes)
+        self.btn_no.clicked.connect(self.no)
+        self.id = 0
         self.money = 50
         self.army = 50
         self.beh = 50
         self.eco = 50
-        px1 = QPixmap('image.jpg')
-        px2 = QPixmap('voenp.jpg')
-        self.image.setPixmap(px1)
-        self.question.setPixmap(px2)
+        self.days = 0
+        self.lose = False
+        self.fill_window()
 
     def paintEvent(self, e):
         self.paint = QPainter()
         self.paint.begin(self)
-        self.paint.drawRect(300, 10, 10, 100)
+        lst = [self.eco, self.beh, self.army, self.money]
+        for i in range(4):
+            self.paint.setBrush(QColor(255, 255, 255))
+            self.paint.drawRect(200 + 20 * i, 10, 10, 50)
+            self.paint.setBrush(QColor(0, 0, 0))
+            self.paint.drawRect(200 + 20 * i, 60 - lst[i] // 2, 10, lst[i] // 2)
         self.paint.end()
 
-    # def yes(self):
-    #
-    # def no(self):
+    def fill_window(self, a=randint(1, 9)):
+        with sqlite3.connect('questions_bd.sqlite') as con:
+            cur = con.cursor()
+            while a == self.id:
+                a = randint(1, 9)
+            self.id = a
+            result = cur.execute(f"""SELECT picture, question
+                                     FROM questions where id = {a}""").fetchall()
+            px3 = QPixmap(f'ends/{result[0][0]}')
+            self.question.setText(result[0][1])
+            self.image.setPixmap(px3)
+
+    def yes(self):
+        if not self.lose:
+            with sqlite3.connect('questions_bd.sqlite') as con:
+                cur = con.cursor()
+                result = cur.execute(f"""SELECT eco_index1, beh_index1, army_index1, money_index1 
+                                         FROM questions where id = {self.id}""").fetchall()
+                a, b, c, d = [int(i) for i in result[0]]
+                self.repaint()
+                self.eco += a
+                self.beh += b
+                self.army += c
+                self.money += d
+                self.update()
+                self.fill_window()
+                self.losing_check()
+                self.days += 1
+                self.days_counter.display(self.days)
+
+    def no(self):
+        if not self.lose:
+            with sqlite3.connect('questions_bd.sqlite') as con:
+                cur = con.cursor()
+                result = cur.execute(f"""SELECT eco_index2, beh_index2, army_index2, money_index2
+                                         FROM questions where id = {self.id}""").fetchall()
+                a, b, c, d = [int(i) for i in result[0]]
+                self.repaint()
+                self.eco += a
+                self.beh += b
+                self.army += c
+                self.money += d
+                self.update()
+                self.fill_window()
+                self.losing_check()
+                self.days += 1
+                self.days_counter.display(self.days)
+
+    def losing_check(self):
+        if 100 <= self.eco >= 0 or 100 <= self.beh >= 0 or 100 <= self.army >= 0 or 100 <= self.money >= 0:
+            self.question.setText('Вы проиграли')
+            self.lose = True
 
 
 app = QApplication(sys.argv)
--- window.py
+++ window.py
@@ -6,34 +6,36 @@ class Ui_Dialog(object):
         Dialog.setObjectName("Dialog")
         Dialog.resize(360, 600)
 
-        self.btn_yes = QtWidgets.QPushButton(Dialog)
-        self.btn_yes.setGeometry(QtCore.QRect(20, 555, 80, 25))
-        self.btn_yes.setObjectName("pushButton")
-
         self.btn_no = QtWidgets.QPushButton(Dialog)
-        self.btn_no.setGeometry(QtCore.QRect(270, 555, 80, 25))
-        self.btn_no.setObjectName("pushButton_2")
+        self.btn_no.setGeometry(QtCore.QRect(20, 555, 80, 25))
+        self.btn_no.setObjectName("pushButton")
+
+        self.btn_yes = QtWidgets.QPushButton(Dialog)
+        self.btn_yes.setGeometry(QtCore.QRect(260, 555, 80, 25))
+        self.btn_yes.setObjectName("pushButton_2")
 
         self.days_counter = QtWidgets.QLCDNumber(Dialog)
-        self.days_counter.setGeometry(QtCore.QRect(10, 10, 160, 100))
+        self.days_counter.setGeometry(QtCore.QRect(10, 10, 160, 70))
         self.days_counter.setObjectName("lcdNumber")
 
         self.image = QtWidgets.QLabel(Dialog)
-        self.image.setGeometry(QtCore.QRect(30, 130, 320, 300))
+        self.image.setGeometry(QtCore.QRect(30, 90, 320, 300))
         self.image.setText("")
         self.image.setObjectName("label")
 
         self.question = QtWidgets.QLabel(Dialog)
-        self.question.setGeometry(QtCore.QRect(30, 460, 300, 70))
+        self.question.setGeometry(QtCore.QRect(30, 410, 300, 90))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.question.setFont(font)
         self.question.setText("")
         self.question.setObjectName("label_2")
-
+        self.question.setWordWrap(True)
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.btn_yes.setText(_translate("Dialog", "Согласиться"))
+        self.btn_no.setText(_translate("Dialog", "Отказаться"))
